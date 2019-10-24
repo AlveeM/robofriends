5c7d5eabd0b029b278ed21e265250ecc6aada2ab
Alvee Akand <9080771+AlveeM@users.noreply.github.com>
2019-10-24 19:40:07 +0600

    Updates

---
+++ precache-manifest.9570d34de50c3c8fd24bd0021d69bc53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7292d9955f9bc0335d2432321df0c020",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "147b5c49b3149bd086cf",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7e449a69470b0739b906",
+    "url": "/robofriends/static/css/main.ed183067.chunk.css"
+  },
+  {
+    "revision": "147b5c49b3149bd086cf",
+    "url": "/robofriends/static/js/2.4a9304e9.chunk.js"
+  },
+  {
+    "revision": "7e449a69470b0739b906",
+    "url": "/robofriends/static/js/main.69cf70aa.chunk.js"
+  },
+  {
+    "revision": "1709d8b2ba992f65501f",
+    "url": "/robofriends/static/js/runtime~main.318ca2b2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
