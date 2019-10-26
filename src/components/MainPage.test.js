0e356b1672c0fb88c3c757d2d39418ddd858f031
Alvee Akand <9080771+AlveeM@users.noreply.github.com>
2019-10-27 00:33:50 +0600

    added reducer tests

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -50,7 +50,9 @@ it('filters robots correctly 2', () => {
         searchField: 'a',
         isPending: false
     }
+
     const filteredRobots = [];
     const wrapper3 = shallow(<MainPage { ...mockProps3 } />);
+
     expect(wrapper3.instance().filterRobots()).toEqual(filteredRobots);
 })
