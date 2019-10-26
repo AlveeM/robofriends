26fd762e92bacc70783bd7337c32e6b7499d1a92
Alvee Akand <9080771+AlveeM@users.noreply.github.com>
2019-10-27 00:09:24 +0600

    added MainPage.js and tests

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
-import Header from '../components/Header';
-// import { robots } from './robots';
-import './App.css';
 import { setSearchField, requestRobots } from '../actions';
+
+import MainPage from '../components/MainPage';
+import './App.css';
 
 const mapStateToProps = state => {
   return {
@@ -26,46 +22,8 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     robots: []
-  //   }
-  // }
-
-  componentDidMount() {
-    this.props.onRequestRobots();
-    // fetch('http://jsonplaceholder.typicode.com/users')
-    // .then(response => {
-    //   return response.json();
-    // })
-    // .then(users => {
-    //   this.setState({ robots: users })
-    // })
-  }
-
-  render() {
-    // const { searchField, onSearchChange } = this.state;
-    const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    })
-
-    if (isPending) {
-      return <h1>Loading</h1>
-    } else {
-      return (
-        <div className='tc'>
-          <Header />
-          <SearchBox searchChange={ onSearchChange }/>
-          <Scroll>
-            <ErrorBoundary>
-              <CardList robots={ filteredRobots }/>
-            </ErrorBoundary>
-          </Scroll>
-        </div>
-      )
-    }
+     render() {
+     return <MainPage { ...this.props } />
   }
 }
 
